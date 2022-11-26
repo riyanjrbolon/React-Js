b666d628a97e76d849d832719a7c92b2341b317b
riyanjrbolon <riyanjrbolon@gmail.com>
2022-11-27 04:58:43 +0700

    first commit

---
+++ src/components/FaqComp.js
@@ -0,0 +1,150 @@
+import React from "react";
+import { Container, Row, Col, Accordion } from "react-bootstrap";
+
+const FaqComp = () => {
+    return (
+        <div className="faq" id="faq">
+            <Container>
+                <Row className="mb-5">
+                    <Col>
+                        <h2 className="text-center fw-bold" data-aos="fadeUp">Yang Biasa Di Tanyakan</h2>
+                        <p className="text-center" data-aos="fadeUp" data-aos-delay="200">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                    </Col>
+                </Row>
+                <Row className="row-cols-lg-2 row-cols-1 g-4">
+                    <Col data-aos="fadeUp" data-aos-delay="400">
+                        <Accordion>
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>Bagaimana Cara Sewa?</Accordion.Header>
+                            <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        </Accordion>
+                    </Col>
+                    <Col data-aos="fadeUp" data-aos-delay="400">
+                        <Accordion>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header>Apakah Tempatnya Nyaman?</Accordion.Header>
+                            <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        </Accordion>
+                    </Col>
+                    <Col data-aos="fadeUp" data-aos-delay="400">
+                        <Accordion>
+                        <Accordion.Item eventKey="2">
+                            <Accordion.Header>Apakah WIFI Gratis?</Accordion.Header>
+                            <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        </Accordion>
+                    </Col>
+                    <Col data-aos="fadeUp" data-aos-delay="400">
+                        <Accordion>
+                        <Accordion.Item eventKey="3">
+                            <Accordion.Header>Bagaimana Lingkungan Sekitarnya?</Accordion.Header>
+                            <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        </Accordion>
+                    </Col>
+                    <Col data-aos="fadeUp" data-aos-delay="400">
+                        <Accordion>
+                        <Accordion.Item eventKey="4">
+                            <Accordion.Header>Sudah Beroperasi Berapa Lama?</Accordion.Header>
+                            <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        </Accordion>
+                    </Col>
+                    <Col data-aos="fadeUp" data-aos-delay="400">
+                        <Accordion>
+                        <Accordion.Item eventKey="5">
+                            <Accordion.Header>Apakah Sudah Terevikasi Dengan Baik?</Accordion.Header>
+                            <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        </Accordion>
+                    </Col>
+                    <Col data-aos="fadeUp" data-aos-delay="400">
+                        <Accordion>
+                        <Accordion.Item eventKey="6">
+                            <Accordion.Header>Berapa Kapasitas Maksimal Penginap?</Accordion.Header>
+                            <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        </Accordion>
+                    </Col>
+                    <Col data-aos="fadeUp" data-aos-delay="400">
+                        <Accordion>
+                        <Accordion.Item eventKey="7">
+                            <Accordion.Header>Apakah Sesuai Dengan Anak - Anak?</Accordion.Header>
+                            <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        </Accordion>
+                    </Col>
+                </Row>
+            </Container>    
+        </div>
+  );
+};
+
+
+export default FaqComp;
